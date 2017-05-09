39632ed3f5041fcc6692f1909eeec14dc109a0cb
Frederik Gelder <frederik.gelder@freenet.de>
2017-05-09 16:16:14 +0200

    added attribute always_inline to the functions, so the compiler doesnt get any crazy ideas (look at the assembly without the attribute. it puts a single nop in an external function, calls that, and then places the other nops right after the call...)

--- fredOptimization.h
+++ fredOptimization.h
@@ -17,12 +17,12 @@ the described methods throw static assertion errors if they detect that you used
 
 // creates an arbitrary amount of nops
 // usage: nop<5>();
-template<int count> inline void nop(){
+template<long count> __attribute__((always_inline)) inline void nop(){
     nop<count - 1>();
     __asm__ __volatile__("nop\n\t");
 }
 
-template<> inline void nop<0>(){
+template<> __attribute__((always_inline)) inline void nop<0>(){
     // do nothing. this is needed so the above nop recursion ends
 }
 
@@ -36,7 +36,7 @@ constexpr uint8_t fredDigitalPinToIndex(const uint8_t pin){
     return (pin < 8) ? pin : (pin < 14) ? pin-8 : (pin >= A0 && pin <= A5) ? pin-A0 : 0;
 }
 
-template<uint8_t pinIndex, uint8_t port> inline void switchOnInPort(){
+template<uint8_t pinIndex, uint8_t port> __attribute__((always_inline)) inline void switchOnInPort(){
     *((volatile uint8_t*)port) |= 1 << pinIndex;
 }
 
@@ -55,40 +55,40 @@ template<uint8_t pinIndex, uint8_t port> inline void switchOffInPort(){
     *((volatile uint8_t*)port) &= ~(1 << pinIndex);
 }
 
-template<> inline void switchOffInPort<0,0>(){
+template<> __attribute__((always_inline)) inline void switchOffInPort<0,0>(){
     // do nothing if port is 0 (aka cant be resolved)
 }
 
 // switches off a pin in 2 cycles
 // usage: switchOff<5>();
-template<uint8_t pin> inline void switchOff(){
+template<uint8_t pin> __attribute__((always_inline)) inline void switchOff(){
     switchOffInPort<fredDigitalPinToIndex(pin),fredDigitalPinToPort(pin)>();
     static_assert(fredDigitalPinToPort(pin) != 0,"pin doesnt resolve to port");
 }
 
-template<uint8_t pinIndex, uint8_t port> inline void switchOnExclusiveInPort(){
+template<uint8_t pinIndex, uint8_t port> __attribute__((always_inline)) inline void switchOnExclusiveInPort(){
     *((volatile uint8_t*)port) = 1 << pinIndex;
 }
 
-template<> inline void switchOnExclusiveInPort<0,0>(){
+template<> __attribute__((always_inline)) inline void switchOnExclusiveInPort<0,0>(){
     // do nothing if port is 0 (aka cant be resolved)
 }
 
 // switches on a pin in 1 cycles. sets other pins of the port to LOW
 // usage: switchOnExclusive<5>();
-template<uint8_t pin> inline void switchOnExclusive(){
+template<uint8_t pin> __attribute__((always_inline)) inline void switchOnExclusive(){
     switchOnExclusiveInPort<fredDigitalPinToIndex(pin),fredDigitalPinToPort(pin)>();
     static_assert(fredDigitalPinToPort(pin) != 0,"pin doesnt resolve to port");
 }
 
 // switches off whole port in 1 cycle
 // usage: switchPortOff<(uint8_t)&PORTB>();
-template<uint8_t port> inline void switchPortOff(){
+template<uint8_t port> __attribute__((always_inline)) inline void switchPortOff(){
     *((volatile uint8_t*)port) = 0;
 }
 
 // switches off the port of a pin in 1 cycle. use this if you only use 1 pin of a port
-template<uint8_t pin> inline void switchOffPortOfPin(){
+template<uint8_t pin> __attribute__((always_inline)) inline void switchOffPortOfPin(){
     switchPortOff<fredDigitalPinToPort(pin)>();
     static_assert(fredDigitalPinToPort(pin) != 0,"pin doesnt resolve to port");
 }

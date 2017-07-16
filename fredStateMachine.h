c4d87f061739f370cf8a3d5580fe8e26dffd434f
Frederik Gelder <frederik.gelder@freenet.de>
2017-07-16 16:02:29 +0200

    added statemachine

---
+++ fredStateMachine.h
@@ -0,0 +1,51 @@
+typedef int(*stateFunction)();
+typedef void(*stateEnterLeaveHandler)(int, int);
+
+const int noState = -1;
+
+template<int statecount> class fredStateMachine {
+public:
+	fredStateMachine() :_state(noState) {}
+
+	void loop() {
+		if (_state != noState) {
+			auto stateFunc = stateFunctions[_state];
+			if (stateFunc) {
+				int newState = stateFunc();
+				if (newState != noState) {
+					auto leaveEnterFunc = stateLeaveHandlers[_state];
+					if (leaveEnterFunc) { leaveFunc(); }
+					_state = newState;
+					leaveEnterFunc = stateEnterHandlers[_state];
+					if (leaveEnterFunc) { leaveEnterFunc(); }
+				}
+			}
+		}
+	}
+
+	int getState() {
+		return _state;
+	}
+
+	void setState(int state) {
+		_state = state;
+	}
+
+	setStateFunction(int state, stateFunction function) {
+		stateFunctions[state] = function;
+	}
+
+	void setStateEnterHandler(int state, stateEnterLeaveHandler handler) {
+		stateEnterHandlers[state] = handler;
+	}
+
+	void setStateLeaveHandler(int state, stateEnterLeaveHandler handler) {
+		stateLeaveHandlers[state] = handler;
+	}
+
+private:
+	stateEnterLeaveHandler stateEnterHandlers[statecount];
+	stateEnterLeaveHandler stateLeaveHandlers[statecount];
+	stateFunction stateFunctions[statecount];
+	int _state;
+}
